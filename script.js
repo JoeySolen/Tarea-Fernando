f430d72f2f3ea6226901e6af2181e5e43e617b87
Jose Moreno <jgmorenor277@gmail.com>
2023-06-05 19:42:23 -0500

    Se agregó la página del chat de tutoría

--- script.js
+++ script.js
@@ -22,3 +22,51 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
     });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const chatLog = document.getElementById("chat-log");
+    const userInput = document.getElementById("user-input");
+    const sendButton = document.getElementById("send-button");
+    
+    sendButton.addEventListener("click", sendMessage);
+    
+    userInput.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+        sendMessage();
+        }
+    });
+    
+    function sendMessage() {
+        const userMessage = userInput.value;
+        userInput.value = "";
+    
+        addMessage("user", userMessage);
+        sendMessageToGPT(userMessage);
+    }
+    
+    function sendMessageToGPT(message) {
+        // Aquí se envía la solicitud para obtener la respuesta
+        // Por simplicidad, aquí simplemente se puso un mensaje de ejemplo del modelo
+    
+        // Respuesta de ejemplo
+        const gptResponse = "¡Hola!, ¿En qué puedo ayudarte?";
+    
+        // Se simula un retraso de respuesta
+        setTimeout(function() {
+        addMessage("gpt", gptResponse);
+        }, 500);
+    }
+    
+    function addMessage(sender, message) {
+        const messageContainer = document.createElement("div");
+        messageContainer.classList.add("message-container");
+        messageContainer.classList.add(sender);
+    
+        const messageText = document.createElement("p");
+        messageText.innerText = message;
+
+        messageContainer.appendChild(messageText);
+    chatLog.appendChild(messageContainer);
+    chatLog.scrollTop = chatLog.scrollHeight;
+}
+});
